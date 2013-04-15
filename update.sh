d7a61d16b1e5f67e05c71e13fb6edd4f5f998c7a
Max David <davmax04@evergreen.edu>
2013-04-15 02:29:02 -0700

    Adding the motd (announcement bar) to the update script

--- update.sh
+++ update.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
+set -e
 
 function updateFiles() {
   # Download and extract the tarball, excluding the files already here locally
-  curl -#L https://github.com/pipecork/cpat-schoolwork/tarball/master | tar -xzv --strip-components 1 --show-transformed --keep-old-files
+  curl -#L https://github.com/pipecork/cpat-schoolwork/tarball/master \
+  | tar -xzv --strip-components 1 --show-transformed --keep-old-files --exclude={README.md}
+}
+
+function motd() {
+  # Print the most recent motd, then remove the file
+  cat MOTD.md | sed '/^#####/,$ d'
+  rm MOTD.md 2> /dev/null
 }
 
 # TODO
 # commit the downloaded files in their own commit
-# implement motd
 # update the actual repo
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   updateFiles
+  motd
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     updateFiles
+    motd
   fi
 fi
-unset updateFiles
+unset updateFiles motd
