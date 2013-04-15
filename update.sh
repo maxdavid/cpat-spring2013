97bf359aae1f5624586a724c9567139e5f5fb579
Max David <davmax04@evergreen.edu>
2013-04-14 17:58:48 -0700

    Oops don't need that regex. There was a tar flag all along...

--- update.sh
+++ update.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
 
-# The list of folders in the repo. need to exclude them... from the exclusion
-FOLDERS='mlearning\|stats\|seminar\|project\|docs'
-# Returns the  path of every file currently in the directly (sans the folders 
-# themselves) as a single, comma-delimited string
-LOCALFILES=$(find . -path ./.git -prune -o -name '*' \
-           | sed 's/^.\///g' \
-           | sed "/\(^.$\)\|^\($FOLDERS\)$/ d" \
-           | sed ':a;N;$!ba;s/\n/,/g' \
-           )
-
-echo $LOCALFILES #debug
-
 function updateFiles() {
   # Download and extract the tarball, excluding the files already here locally
-  curl -#L https://github.com/pipecork/cpat-schoolwork/tarball/master | tar -xzv --strip-components 1 --show-transformed --exclude={'$LOCALFILES'}
+  curl -#L https://github.com/pipecork/cpat-schoolwork/tarball/master | tar -xzv --strip-components 1 --show-transformed --keep-old-files
 }
 
 # TODO
