2045c08babaddcf7112c37e3bfd484e2e356832c
Max David <davmax04@evergreen.edu>
2013-04-16 01:02:25 -0700

    Update creates a commit for added files

--- update.sh
+++ update.sh
@@ -3,10 +3,8 @@ cd "$(dirname "${BASH_SOURCE}")"
 set -e
 
 MOTD=MOTD.txt
-LOCALFILES=""
 OLDLS="/tmp/old_listing"
 NEWLS="/tmp/new_listing"
-DIFFLS=""
 
 function getNewFiles() {
 # Download and extract the tarball, excluding the files already here locally
@@ -25,19 +23,24 @@ function getListing() {
 
 function commitUpdate() {
 # Commit the new files into their own commit
+  git add $1
   git commit -m"test commit" $1  
 }
 
 function updateFiles() {
-# Download
+# Compare the file listing before and after downloading new files to see if a
+# new commit of updates is needed
   getListing
   echo "$LOCALFILES" | sort > $OLDLS
   getNewFiles
   getListing
   echo "$LOCALFILES" | sort > $NEWLS
   DIFFLS=$(comm -13 $OLDLS $NEWLS)
-  rm $OLDLS 2> /dev/null
-  rm $NEWLS 2> /dev/null
+  rm $OLDLS $NEWLS 2> /dev/null
+  if [ ! -z $DIFFLS ]; then
+    commitUpdate "$DIFFLS"
+  fi
+  unset LOCALFILES DIFFLS
 }
 
 function motd() {
@@ -46,10 +49,8 @@ function motd() {
   #rm $1 2> /dev/null
 }
 
-# TODO
-# commit the downloaded files in their own commit
-# update the actual repo
 
+# Main
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   updateFiles
   motd $MOTD
@@ -58,8 +59,8 @@ else
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     updateFiles
-    # commitUpdate $DIFFLS
     motd $MOTD
   fi
 fi
-unset updateFiles motd
+unset updateFiles MOTD OLDLS NEWLS DIFFLS
+
